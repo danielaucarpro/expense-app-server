4407264dd33424eef9e909bcdb361ff5762053a9
danielaucarpro <danielaucarpro@gmail.com>
2021-12-07 12:15:51 -0800

    created a GET transaction route

--- routes/post.js
+++ routes/post.js
@@ -44,4 +44,27 @@ router.get('/shareExpenses', async (req, res) => {
     //post the transaction in everybody in the list and divide the amount value by the list lenght
 });
 
+//get user's transactions
+router.get('/getTransactions', async (req, res) => {
+    //getting user to return transaction
+    const token = req.headers['x-access-token'];
+    console.log(token, 'token');
+    //using token to check if is the same user who is posting
+    const userEmail = jwt.decode(token);
+    const emailToUse = userEmail.email;
+    const transactions = userEmail.transaction;
+    console.log(emailToUse, 'email');
+    console.log(transactions, 'transactions');
+    //checking if the user exist
+    const checkUser = await User.findOne({ email: emailToUse });
+    console.log(checkUser.transaction, 'transactions');
+
+    if (checkUser) {
+        return res.status(200).json({
+            message: "Data laoded successfully",
+            data: checkUser.transaction
+        });
+    }
+});
+
 module.exports = router;
